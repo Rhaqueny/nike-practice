1a92bf2033d2998dcb76164f6dc47e4929768306
Rhaqueny <Rhaqueny@gmail.com>
2024-09-09 21:53:14 -0400

    Add files via upload

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from '../assets/images'
 
 const Hero = () => {
   return (
@@ -17,13 +18,23 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
         </p>
-        <h1 className="mt-10">
-          <span>The New Arrival</span>
+        <h1
+          className="mt-10 font-palanquin text-8xl max-sm:text-[72px]
+        max-sm:leading-[82] font-bold"
+        >
+          <span
+            className="xl:bg-white xl:whitespace-nowrap 
+          relative z-10 pr-10"
+          >
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span>
-          Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
-        <p>
+        <p
+          className="font-montserrat text-slate-gray text-lg 
+          leading-8 mt-6 mb-14 sm:max-w-sm"
+        >
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
@@ -34,8 +45,29 @@ const Hero = () => {
         >
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center 
+      items-center xl:min-h-screen max-xl:py-40 bg-primary
+      bg-hero bg-cover bg-center">
+        <img 
+          src={bigShoe1}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div>
+          {shoes.map((shoe, index) => (
+            <div>
+              
             </div>
           ))}
         </div>
