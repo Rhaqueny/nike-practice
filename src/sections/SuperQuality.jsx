f3ee3415d8d23e36b494a2d164b7fdad788d1589
Rhaqueny <Rhaqueny@gmail.com>
2024-09-14 15:00:33 -0400

    Add files via upload

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
 
 const SuperQuality = () => {
   return (
-    <div>
-      SuperQuality
-    </div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center 
+      max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You <span className="text-coral-red">Super</span>{" "}
+          <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+        <p
+          className="font-montserrat text-slate-gray text-lg 
+          leading-8 mt-6 mb-14 sm:max-w-sm"
+        >
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="View details" iconURL={arrowRight} />
+      </div>
+    </section>
+  );
+};
 
-export default SuperQuality
+export default SuperQuality;
