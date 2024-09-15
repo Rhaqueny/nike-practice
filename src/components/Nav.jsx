cf3a488a9cb5692a824596281f9139e337a61677
Rhaqueny <Rhaqueny@gmail.com>
2024-09-15 15:15:45 -0400

    Add files via upload

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,7 +24,7 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
+        <div className="hidden max-lg:block cursor-not-allowed">
           <img src={hamburger} alt="Hamburger" width={25} height={25} />
         </div>
       </nav>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../components/Button";
-import { arrowRight } from "../assets/icons";
+import { shoe8 } from '../assets/images'
 
 const SuperQuality = () => {
   return (
@@ -15,13 +15,27 @@ const SuperQuality = () => {
           <span className="text-coral-red">Quality</span> Shoes
         </h2>
         <p
-          className="font-montserrat text-slate-gray text-lg 
-          leading-8 mt-6 mb-14 sm:max-w-sm"
+          className="mt-4 lg:max-w-lg info-text"
         >
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+          Ensuring premium comfort and style, our meticulously crafted
+          footwear is designed to elevate your experience, providing
+          you with unmatched quality, innovation, and a touch of elegance.
         </p>
-        <Button label="View details" iconURL={arrowRight} />
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction.
+        </p>
+        <div className="mt-11">
+          <Button label="View details"/>
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img 
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
       </div>
     </section>
   );
